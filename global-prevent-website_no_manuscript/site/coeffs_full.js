5eb7872033b8c9c14e96c40d1d3fffc0fd0cc9aa
hnidkarim <hnid68@gmail.com>
2025-09-02 08:33:39 +0100

    Décompression du site et suppression du fichier ZIP

---
+++ global-prevent-website_no_manuscript/site/coeffs_full.js
@@ -0,0 +1,17 @@
+/* Placeholder for official PREVENT™ β, interactions, S0.
+   Example structure (to be filled with real coefficients from AHA/ACC or preventr R package): */
+window.COEFFS_FULL = {
+  female: {
+    CVD: {
+      "10": { S0: 0.0, beta: {/* ... */}, beta_ageInt: {/* ... */} },
+      "30": { S0: 0.0, beta: {/* ... */}, beta_ageInt: {/* ... */} }
+    },
+    ASCVD: {},
+    HF: {}
+  },
+  male: {
+    CVD: {},
+    ASCVD: {},
+    HF: {}
+  }
+};
